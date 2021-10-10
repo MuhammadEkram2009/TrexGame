24fa27004c2525688bff2472b96d7eaf5d9c4f97
MuhammadEkram2009 <91430203+MuhammadEkram2009@users.noreply.github.com>
2021-10-10 11:43:33 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,35 +1,39 @@
 
-var trex ,trex_running;
-var ground,groundImage
+  var trex ,trex_running;
+  var ground,groundImage,invisibleGround;
+
 
 function preload(){
   trex_running=loadAnimation("trex1.png","trex3.png","trex4.png")
-groundImage = loadImage("ground2.png")
+  groundImage = loadImage("ground2.png")
 }
 
 function setup(){
   createCanvas(600,200)
   
-  //create a trex sprite
- trex=createSprite(50,160,20,50)
- trex.addAnimation("running",trex_running)
- trex.scale=0.4
- ground=createSprite(300,180,600,20);
- ground.addImage("ground",groundImage)
- edges = createEdgeSprites();
+    //create a trex sprite
+  trex=createSprite(50,160,20,50)
+  trex.addAnimation("running",trex_running)
+  trex.scale=0.4
+  
+  ground=createSprite(300,180,600,20);
+  ground.addImage("ground",groundImage)
+  invisibleGround = createSprite(300,190,600,10)
+  invisibleGround.visible = false
+  edges = createEdgeSprites();
 }
 
 function draw(){
   background(190)
-  if(keyDown("space")){
+  if(keyDown("space")&& trex.y >= 150){
     trex.velocityY = -8
   }
   trex.velocityY = trex.velocityY+0.5
   ground.velocityX = -4
-if(ground.x<0){
-  ground.x = ground.width/2
-}
- trex.collide(ground)
-drawSprites()
+  if(ground.x<0){
+    ground.x = ground.width/2
+  }
+ trex.collide(invisibleGround)
+  drawSprites()
 
 }
