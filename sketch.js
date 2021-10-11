b507a3acb2113e5be5f6e65890b07ca583285e1d
MuhammadEkram2009 <91430203+MuhammadEkram2009@users.noreply.github.com>
2021-10-11 10:38:43 -0600

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,11 +1,12 @@
 
   var trex ,trex_running;
   var ground,groundImage,invisibleGround;
-
+  var cloud,cloudImage;
 
 function preload(){
   trex_running=loadAnimation("trex1.png","trex3.png","trex4.png")
   groundImage = loadImage("ground2.png")
+  cloudImage = loadImage("cloud.png")
 }
 
 function setup(){
@@ -18,9 +19,12 @@ function setup(){
   
   ground=createSprite(300,180,600,20);
   ground.addImage("ground",groundImage)
+  
   invisibleGround = createSprite(300,190,600,10)
   invisibleGround.visible = false
-  edges = createEdgeSprites();
+
+  var rand = Math.round(random(10,60))
+  console.log(rand)
 }
 
 function draw(){
@@ -34,6 +38,20 @@ function draw(){
     ground.x = ground.width/2
   }
  trex.collide(invisibleGround)
+ spawnClouds()
   drawSprites()
+
+}
+function spawnClouds(){
+  if(frameCount% 34 === 0)
+  {
+    cloud = createSprite(600,100,40,12)
+    cloud.addImage(cloudImage)
+    cloud.y = Math.round(random(10,100))
+    cloud.scale = 0.5
+    cloud.velocityX = -5
+    cloud.depth = trex.depth
+    trex.depth = trex.depth + 1
+  }
 
 }
